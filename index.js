9041bf21d1cf3a4f65e5157b2ed41950aa67a1ba
Satyam Dabral <96922072+satyamdabral@users.noreply.github.com>
2023-08-20 13:54:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("music/tom-1.mp3");
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio("music/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio("music/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("music/tom-4.mp3");
+                tom4.play();
+                break;
+
+            case "j":
+                var kick = new Audio("music/snare.mp3");
+                kick.play();
+                break;
+
+            case "k":
+                var snare = new Audio("music/crash.mp3");
+                snare.play();
+                break;
+
+            case "l":
+                var crash = new Audio("music/kick-bass.mp3");
+                crash.play();
+                break;
+
+
+
+
+            default: console.log(buttonInnerHTML);
+
+
+        }
+
+
+
+
+
+
+    });
+}
+
+
+
+
+

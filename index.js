8e2547eb51ee2dd70b99a03559cf301381fed100
Satyam Dabral <96922072+satyamdabral@users.noreply.github.com>
2023-08-20 14:12:33 +0530

    index.js

--- index.js
+++ index.js
@@ -8,37 +8,37 @@ for (var i = 0; i < numberOfDrums; i++) {
         var buttonInnerHTML = this.innerHTML;
         switch (buttonInnerHTML) {
             case "w":
-                var tom1 = new Audio("music/tom-1.mp3");
+                var tom1 = new Audio("tom-1.mp3");
                 tom1.play();
                 break;
 
             case "a":
-                var tom2 = new Audio("music/tom-2.mp3");
+                var tom2 = new Audio("tom-2.mp3");
                 tom2.play();
                 break;
 
             case "s":
-                var tom3 = new Audio("music/tom-3.mp3");
+                var tom3 = new Audio("tom-3.mp3");
                 tom3.play();
                 break;
 
             case "d":
-                var tom4 = new Audio("music/tom-4.mp3");
+                var tom4 = new Audio("tom-4.mp3");
                 tom4.play();
                 break;
 
             case "j":
-                var kick = new Audio("music/snare.mp3");
+                var kick = new Audio("snare.mp3");
                 kick.play();
                 break;
 
             case "k":
-                var snare = new Audio("music/crash.mp3");
+                var snare = new Audio("crash.mp3");
                 snare.play();
                 break;
 
             case "l":
-                var crash = new Audio("music/kick-bass.mp3");
+                var crash = new Audio("kick-bass.mp3");
                 crash.play();
                 break;
 
